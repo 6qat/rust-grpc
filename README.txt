272ee21ddf62de75157a1a056fffe00033eda603
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2021-08-09 23:29:39 -0300

    Add readme file

---
+++ README.txt
@@ -0,0 +1,4 @@
+(https://www.oreilly.com/library/view/rust-programming-by/9781788390637/e07dc768-de29-482e-804b-0274b4bef418.xhtml)
+rustup default nightly
+rustup default stable
+
--- src/client.rs
+++ src/client.rs
@@ -10,7 +10,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = GreeterClient::connect("https://[::1]:50051").await?;
 
     let request = tonic::Request::new(HelloRequest {
-        name: "Tonic".into(),
+        name: "Mr. Tonic".into(),
     });
 
     let response = client.say_hello(request).await?;
