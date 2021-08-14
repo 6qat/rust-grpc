3258d56dbfa21a16fb9e7344820f5d61adf7095e
Guilherme C. B. Moreira <ceschiatti@gmail.com>
2021-08-14 20:12:48 -0300

    Playing with GRPC

--- build.rs
+++ build.rs
@@ -1,5 +1,20 @@
+#[allow(unused_imports)]
+use std::env;
+use std::path::{PathBuf, Path};
+
+#[allow(unused_variables)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/helloworld.proto")?;
-    tonic_build::compile_protos("proto/dtc.proto")?;
+    let grpc_health_v1_descriptor_set_path: PathBuf =
+        PathBuf::from("out/").join("grpc_health_v1.bin");
+
+    let out_dir = Path::new("out/");
+    tonic_build::configure()
+        .out_dir(out_dir)
+        .file_descriptor_set_path(grpc_health_v1_descriptor_set_path)
+        .build_server(true)
+        .build_client(true)
+        .format(true)
+        .compile(&["proto/dtc.proto", "proto/helloworld.proto"], &["proto/"])?;
+
     Ok(())
 }
